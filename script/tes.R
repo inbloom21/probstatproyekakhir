0ac608977ee3fd76ab403dac4f0c7afc85b18d06
Raihan466 <raihanramadhan267@gmail.com>
2026-06-01 17:49:49 +0700

    Bagian Raihan

--- script/tes.R
+++ script/tes.R
@@ -1,1 +1,17 @@
-Rafanita Fatih
+library(dplyr)
+
+# memahami dataset
+data <- read.csv("dataset/pembangunan_wilayah_missing_outlier.csv")
+str(data)
+head(data)
+dim(data)
+
+# statistika deksriptif
+summary(data)
+data %>%
+  summarise(
+    rata_kemiskinan = mean(kemiskinan, na.rm = TRUE),
+    median_kemiskinan = median(kemiskinan, na.rm = TRUE),
+    sd_kemiskinan = sd(kemiskinan, na.rm = TRUE)
+  )
+
